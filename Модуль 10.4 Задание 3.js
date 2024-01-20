188bc7e8976c941fa206630ac9c33166532b9143
Nick199213 <151787471+Nick199213@users.noreply.github.com>
2024-01-20 15:11:18 +0300

    Add files via upload

---
+++ Модуль 10.4 Задание 3.js
@@ -0,0 +1,6 @@
+let myDream = 'Hello'
+
+let allSymbols = myDream.split("")
+let reverseallSymbols = allSymbols.reverse()
+let joinallSymbols = reverseallSymbols.join("")
+console.log(joinallSymbols);
