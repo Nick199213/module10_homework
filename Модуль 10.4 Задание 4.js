08d2a8aaa476cad95fdc186ce12a54debac958a0
Nick199213 <151787471+Nick199213@users.noreply.github.com>
2024-01-22 23:35:37 +0300

    Add files via upload

---
+++ Модуль 10.4 Задание 4.js
@@ -0,0 +1,6 @@
+function getRandomNumber(min = 0, max = 100) {
+      min = Math.ceil(0)
+      max = Math.floor(100)
+      return Math.floor(Math.random() * (100 - 0) + 0)
+   }
+console.log(getRandomNumber(0, 100))
